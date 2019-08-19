7d7f7387bab8bbbaaedb04f8ba07fd1b9e0e2b3d
franckoline@hotmail.com <franckoline@hotmail.com>
2019-08-19 12:59:43 +0100

    PayOla

---
+++ src/conditions.py
@@ -0,0 +1,37 @@
+# n = 2
+# ## If Conditions
+# if n < 0:
+#     print(
+#         "Hello Word"
+#     )
+#
+# # If else condition
+# if n <= 0:
+#     result = n**2
+#     print(
+#         result
+#     )
+# else:
+#     result = n*23
+#     print(
+#         result
+#     )
+
+## Boolean Value
+
+# result = 3
+#
+# print(result == n)
+
+## Nested Loop
+x =49
+y = 40
+if x == y:
+    print('x and y are equal')
+else:
+    if x < y:
+        print('x is less than y')
+    else:
+        print('x is greater than y')
+
+
