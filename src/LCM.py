56520477089749ddc75984553003b1fa2ff02fe3
nathanbrand12 <nathanbrand12@gmail.com>
2019-09-10 17:48:17 +0200

    LCM.py

--- src/LCM.py
+++ src/LCM.py
@@ -1,16 +1,37 @@
-def lcm(x, y):
-  if x > y:
-        greater = x
-  else:
-        greater = y
-  while(True):
-     if((greater % x == 0) and (greater % y == 0)):
-            lcm = greater
-            break
-     greater += 1
-  return lcm
+# def lcm(x, y):
+#   if x > y:
+#         greater = x
+#   else:
+#         greater = y
+#   while(True):
+#      if((greater % x == 0) and (greater % y == 0)):
+#             lcm = greater
+#             break
+#      greater += 1
+#   return lcm
+#
+# num1 = float(input("Input first number: "))
+# num2 = float(input("Input second number: "))
+# result = lcm(num1, num2)
+# print(result)
 
-num1 = float(input("Input first number: "))
-num2 = float(input("Input second number: "))
-result = lcm(num1, num2)
-print(result)
+
+## N FACTORIAL
+
+# def factorial(x):
+#     if x == 0:
+#         return 1
+#     elif x == 1:
+#         return 1
+#     elif x < 0:
+#         print("Invalid! Negative numbers cannot be computed")
+#     else:
+#         return x * factorial(x-1)
+#
+# x=int(input("Input number: "))
+# print(factorial(x))
+
+
+
+
+
